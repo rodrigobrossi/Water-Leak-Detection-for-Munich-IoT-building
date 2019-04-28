963e3dda0569fe5cc4fa793b123edba529894609
cyj <cyjiang@us.ibm.com>
2019-04-28 11:19:23 +0200

    change year

--- BuildingDamageProtection/src/main/python/bdp_auth.py
+++ BuildingDamageProtection/src/main/python/bdp_auth.py
@@ -2,7 +2,7 @@
 # IBM Confidential
 # OCO Source Materials
 #
-#  (C) Copyright IBM Corp. 2018
+#  (C) Copyright IBM Corp. 2019
 #
 # The source code for this program is not published or otherwise
 # divested of its trade secrets, irrespective of what has
