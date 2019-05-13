3475636ad59ec820ab4cf8766f2eec39b99c3019
Alona Sakhnenko <alona.sakhnenko@ibm.com>
2019-05-13 12:35:23 +0200

    Incident detection detection (threshholding) #7

--- BuildingDamageProtection/src/main/python/bdp_notifier.py
+++ BuildingDamageProtection/src/main/python/bdp_notifier.py
@@ -9,33 +9,52 @@
 # been deposited with the U.S. Copyright Office.
 #############################################################
 import datetime, json
+import pandas as pd
 import ibm_db
+
 import bdp_dbutil
 from bdp_property import BDPProperty
+from bdp_incident import BDPIncident
 
 class BDPNotifier():
-
-    def hardwareCallback(event):
-        print("[BDPNotifier] msg from device [%s]: %s" % (event.device, json.dumps(event.data)))
-        try:
-            conn = bdp_dbutil.BDPDBConnection.getInstance().getDBConnection()
-
-            timestamp = datetime.datetime.now().strftime("%Y-%m-%d-%H.%M.%S")
-            hardware_uid = bdp_dbutil.getHardwareByDevice(conn, event.device)["HARDWARE_UID"]
-
-            sql_string = "INSERT INTO " + bdp_dbutil.getTableName("BDP_RAW_EVENTS") + "(READING_TIME, READING, HARDWARE_UID) VALUES ('" 
-            sql_string += str(timestamp) + "', '" + json.dumps(event.data) + "', '" + str(hardware_uid)  + "')"
-            print("[BDPNotifier] Inserting to DB: {}".format(sql_string))
-            
-            stmt = ibm_db.exec_immediate(conn, sql_string)
-            if ibm_db.num_rows(stmt) == 0:
-                print("[BDPNotifier] Could not add the event to DB!")
-
-            BDPNotifier.processEvent(conn, hardware_uid)
-            
-        except Exception as e:
-            print(e)
     
-    def processEvent(conn,hardware_uid):
-        print(bdp_dbutil.getRawEventsByHardwareUID(conn, hardware_uid))
+    def getIncidentOccurence(conn, hardware):
+        # Read all the values
+        table = pd.DataFrame(bdp_dbutil.getRawEventsByHardwareUID(conn, hardware["HARDWARE_UID"]))
+        # TODO: Find a better way
+        # Consider only the last 2 h
+
+        table = table.tail(4 * 120)
+        table.reset_index(drop=True, inplace=True)
+        table.sort_values(by=['READING_TIME'],minplace=True)
+
+        # Get important values
+        table['HUMIDITY'] = None
+        for i in range(table.shape[0]):
+            hardware_json = json.loads(table.READING.iloc[i])
+            table.at[i, 'HUMIDITY']  = hardware_json['humidity']
+       
+        value_of_interest = table.HUMIDITY.iloc[-1]
+        if value_of_interest < 20:
+            return None
+
+        # TODO: check the slope
+        response = {}
+        response['INCIDENT_DETAIL'] = hardware
+        response['INCIDENT_DETAIL']['URGENCY'] = 'MODERATE' if value_of_interest < 75 else 'CRITICAL'
+        response['INCIDENT_TIME'] = str(table.READING_TIME.iloc[-1])
+        response['TENANT_ID'] = hardware['TENANT_ID']
+
+        print('[BDPNotifier] response: {}'.format(response))
+        return response
+        
+    def handleEvents(conn, hardware_uid):
+        # Process the humidity values
+        incident = BDPNotifier.getIncidentOccurence(conn, hardware_uid)
+        if incident is None:
+            # No incident occured
+            return
+        # Post an incident
+        response = BDPIncident.post(incident)
+        print('[BDPNotifier] incident response: {}'.format(response))
         
