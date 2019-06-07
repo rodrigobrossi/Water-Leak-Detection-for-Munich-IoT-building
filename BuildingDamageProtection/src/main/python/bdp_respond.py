070f985e964d0b30cbc04c62621e8724d5c967a4
joanneclarke <joanne.clarke@oniqua.com>
2019-06-07 13:57:42 +0200

    Separated building template context from get method to make it testable

--- BuildingDamageProtection/src/main/python/bdp_respond.py
+++ BuildingDamageProtection/src/main/python/bdp_respond.py
@@ -31,66 +31,72 @@ class BDPIncidentRespond(Resource):
         Generates UI web page based on notification id parameter
         """
         try:
-            nid = request.args.get('nid')
-            notification = bdp_dbutil.getNotificationByNotificationID(nid)
-            user = bdp_dbutil.getUserByUserID(notification["USER_ID"])
 
-            incident_id = bdp_dbutil.getNotificationByNotificationID(nid)["INCIDENT_ID"]
-            incident = bdp_dbutil.getIncidentByIncidentID(incident_id)
-            print('[BDPIncidentRespond] {}'.format(incident))
-            # TODO: Fix original incident assignment
-            #original_incident = bdp_dbutil.getIncidentByIncidentID(conn, incident['INCIDENT_ID_ORIGINAL'])
-            original_ts = incident['INCIDENT_TIME']
+            context = BDPIncidentRespond.buildContext(request)
 
-            tenant = bdp_dbutil.getTenantByTenantID(incident['TENANT_ID'])
-            tenant_name = tenant['TENANT_NAME']
-
-            users_group = bdp_dbutil.getUsersWithNIDs(tenant['TENANT_ID'])
-
-            users_names = []
-            for user in users_group:
-                users_names.append(user['USER_NAME'])
-
-            status_code = {
-                1: 'Resolved',
-                2: 'New',
-                3: 'In progress'
-            }
-
-            incident_status = status_code[incident['INCIDENT_STATUS_CODE']]
-            incident_detail = json.loads(incident['INCIDENT_DETAIL'])
-            urgency = incident_detail['URGENCY']
-            hardware = bdp_dbutil.getHardwareByHardwareUID(incident['CAUSE_HARDWARE'])
-
-            handler = 'Not assigned'
-            if 'RESPONDER' in incident_detail.keys():
-                handler_id = incident_detail['RESPONDER']
-                handler = bdp_dbutil.getUserByUserID(handler_id)['USER_NAME']
-
-            bdp_util.createPlot(hardware['HARDWARE_UID'])
-
-            resp = make_response(render_template('respond.html', 
-                                                name = user['USER_NAME'],
-                                                tenant = tenant_name,
-                                                sensor_id = hardware['HARDWARE_ID'],
-                                                sensor_type = hardware['HARDWARE_TYPE'],
-                                                hardware_uid = hardware['HARDWARE_UID'],
-                                                location = hardware['HARDWARE_DETAIL'],
-                                                timestamp = original_ts,
-                                                status = incident_status,
-                                                handler = handler,
-                                                urgency_vis_1 = 'visible',
-                                                urgency_vis_2 = 'visible',
-                                                urgency_vis_3 = 'visible' if urgency=='critical' else 'hidden',
-                                                users = users_names,
-                                                users_amount = len(users_names)
-                                                ))
+            resp = make_response(render_template('respond.html', **context))
             resp.headers['Content-type'] = 'text/html; charset=utf-8'
 
             return resp
+
         except Exception as e:
             print(e)
             return {"result":"fail", "msg": str(e)}, 400
+
+    def buildContext(request):
+
+        nid = request.args.get('nid')
+        notification = bdp_dbutil.getNotificationByNotificationID(nid)
+        user = bdp_dbutil.getUserByUserID(notification["USER_ID"])
+
+        incident_id = bdp_dbutil.getNotificationByNotificationID(nid)["INCIDENT_ID"]
+        incident = bdp_dbutil.getIncidentByIncidentID(incident_id)
+        print('[BDPIncidentRespond] {}'.format(incident))
+        # TODO: Fix original incident assignment
+        #original_incident = bdp_dbutil.getIncidentByIncidentID(conn, incident['INCIDENT_ID_ORIGINAL'])
+        original_ts = incident['INCIDENT_TIME']
+
+        tenant = bdp_dbutil.getTenantByTenantID(incident['TENANT_ID'])
+        tenant_name = tenant['TENANT_NAME']
+
+        users_group = bdp_dbutil.getUsersWithNIDs(tenant['TENANT_ID'])
+
+        users_names = []
+        for user in users_group:
+            users_names.append(user['USER_NAME'])
+
+        status_code = {
+            1: 'Resolved',
+            2: 'New',
+            3: 'In progress'
+        }
+
+        incident_status = status_code[incident['INCIDENT_STATUS_CODE']]
+        incident_detail = json.loads(incident['INCIDENT_DETAIL'])
+        urgency = incident_detail['URGENCY']
+        hardware = bdp_dbutil.getHardwareByHardwareUID(incident['CAUSE_HARDWARE'])
+
+        handler = 'Not assigned'
+        if 'RESPONDER' in incident_detail.keys():
+            handler_id = incident_detail['RESPONDER']
+            handler = bdp_dbutil.getUserByUserID(handler_id)['USER_NAME']
+
+        bdp_util.createPlot(hardware['HARDWARE_UID'])
+
+        return {'name': user['USER_NAME'],
+                'tenant' : tenant_name,
+                'sensor_id' : hardware['HARDWARE_ID'],
+                'sensor_type' : hardware['HARDWARE_TYPE'],
+                'hardware_uid' : hardware['HARDWARE_UID'],
+                'location' : hardware['HARDWARE_DETAIL'],
+                'timestamp' : original_ts,
+                'status' : incident_status,
+                'handler' : handler,
+                'urgency_vis_1' : 'visible',
+                'urgency_vis_2' : 'visible',
+                'urgency_vis_3' : 'visible' if urgency=='critical' else 'hidden',
+                'users' : users_names,
+                'users_amount' : len(users_names)}
 
     def post(self):
         """
@@ -98,7 +104,7 @@ class BDPIncidentRespond(Resource):
         """
         print("[BDPIncidentRespond] post request received")
         try:
-            request_json = request.get_json(force=True)
+            request_json : request.get_json(force=True)
             nid = str(request.referrer).split('?')[1][4:]
 
             action = request_json['ACTION']
